50e3914f393de52c782daed865d55128296f6fd4
akat03 <kato@econosys-system.com>
2025-12-26 17:15:39 +0900

    Update goreleaser configuration and GitHub Actions for versioning; adjust formula path and test command

--- main.go
+++ main.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    
-    "github.com/kato/fastrun/cmd"
+	"fmt"
+	"os"
+
+	"github.com/kato/fastrun/cmd"
 )
 
 var (
-    version = "dev"
-    commit  = "unknown"
-    date    = "unknown"
+	version = "dev"
+	commit  = "unknown"
+	date    = "unknown"
 )
 
 func main() {
-    cmd.SetVersionInfo(version, commit, date)
-    if err := cmd.Execute(); err != nil {
-        fmt.Fprintln(os.Stderr, err)
-        os.Exit(1)
-    }
+	cmd.SetVersionInfo(version, commit, date)
+	if err := cmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
